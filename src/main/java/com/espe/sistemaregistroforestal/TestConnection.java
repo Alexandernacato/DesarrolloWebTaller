25abadc77fcd75811e7e19238bf84a7d71b25e13
Alexander Ñacato <alexandernacato@gmail.com>
2025-05-10 17:53:50 -0500

    Arquitectura de la Web MVC

---
+++ src/main/java/com/espe/sistemaregistroforestal/TestConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espe.sistemaregistroforestal;
+
+import com.espe.sistemaregistroforestal.dao.ConnectionBdd;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alexa
+ */
+public class TestConnection {
+      public static void main(String[] args) {
+        try (Connection conn = ConnectionBdd.getConexion()) {
+            if (conn != null) {
+                System.out.println("Conexión exitosa a la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos.");
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ target/SistemaRegistroForestal-1.0-SNAPSHOT/Activities.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Activities
+    Created on : 10 may. 2025, 17:35:45
+    Author     : alexa
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
